d65b17825600f4d72a0bcd2c94dc906d11854552
GyetvaiAdam <adamgyetvai26@gmail.com>
2024-09-04 09:19:05 +0200

    folyt

--- js/app.js
+++ js/app.js
@@ -1,17 +1,35 @@
-// alert("asdfa")
+// számokat tartalmazó négyzetek létrehozása:
+const makeBoxes = () =>{
+    const data = [
+        {number: 1},
+        {number: 2},
+        {number: 3},
+        {number: 4},
+        {number: 5},
+        {number: 6},
+        {number: 7},
+        {number: 8},
+        {number: 9},
+        {number: 10},
+        {number: 11},
+        {number: 12},
+        {number: 13},
+        {number: 14},
+        {number: 15}
+    ];
+    const content = data.map()
+}
 
-const makeBoxes = () => {
-    let boxList = [];
-    for (let i = 1; i < 16; i++) {
-        boxList.push(`<div id="box-${i}" class="box">${i}</div>`);
-    }
-    return boxList;
-};
-
+// A négyzetek megjelenítése a html-ben:
 const renderBoxes = () => {
     const boxesContainer = document.querySelector(".boxes");
     boxesContainer.innerHTML = "";
     boxesContainer.innerHTML = makeBoxes().join('');
 };
 
-document.addEventListener("DOMContentLoaded", renderBoxes);
+//console.log("Boxlista: ", document.querySelectorAll(".box"))
+
+document.addEventListener("DOMContentLoaded", () =>{
+    renderBoxes()
+    console.log("Boxlista: ", document.querySelectorAll(".box"))
+});
